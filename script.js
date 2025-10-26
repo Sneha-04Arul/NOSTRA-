89142b055180be4baab57ff96bd39579043d4ac0
A Sneha <sneha04arul@gmail.com>
2025-10-26 19:01:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.remove("active");
+    if (i === index) slide.classList.add("active");
+  });
+}
+
+function changeSlide(direction) {
+  currentSlide += direction;
+  if (currentSlide < 0) currentSlide = slides.length - 1;
+  if (currentSlide >= slides.length) currentSlide = 0;
+  showSlide(currentSlide);
+}
+
+setInterval(() => {
+  changeSlide(1);
+}, 5000);
+
+showSlide(currentSlide);
